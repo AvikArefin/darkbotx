9db25e55c1e99d92f7f3ef143769361d33b01c6b
avikarefin <avikarefin5@gmail.com>
2026-02-15 03:26:06 +0600

    fix: testing model

--- play.py
+++ play.py
@@ -1,17 +1,17 @@
 import torch
 import genesis as gs
 from code.env import FastFrankaEnv
-from code.wrapper import FrankaRSLWrapper
+
 from rsl_rl.runners import OnPolicyRunner
 from main import train_cfg
 
 def test_model(checkpoint_path):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "mps:0" if torch.backends.mps.is_available() else "cpu")
- 
-    # 1. Initialize Environment for Visualization
-    env_wrapped = FrankaRSLWrapper(FastFrankaEnv(device=device, n_envs=1, show_viewer=True))
+    device = "cuda:0" if torch.cuda.is_available() else "mps:0" if torch.backends.mps.is_available() else "cpu:0"
     
-    runner = OnPolicyRunner(env_wrapped, train_cfg, log_dir="logs/", device=device)
+    # 1. Initialize Environment for Visualization
+    env = FastFrankaEnv(n_envs=1, show_viewer=True)
+    
+    runner = OnPolicyRunner(env, train_cfg, log_dir="logs/", device=device)
     
     print(f"Loading checkpoint from: {checkpoint_path}")
     runner.load(checkpoint_path)
@@ -20,7 +20,7 @@ def test_model(checkpoint_path):
     policy = runner.get_inference_policy(device=device)
 
     # 3. Inference Loop
-    obs, _ = env_wrapped.reset()
+    obs, _ = env.reset()
     
     print("Starting inference... Press Ctrl+C to stop.")
     for i in range(2000):
@@ -28,12 +28,12 @@ def test_model(checkpoint_path):
         actions = policy(obs)
         
         # Step the environment
-        obs, rewards, dones, infos = env_wrapped.step(actions)
+        obs, rewards, dones, infos = env.step(actions)
         
         # Optional: slow down the loop so you can actually see the movement
-        gs.tools.sleep(0.01) 
+        # gs.tools.sleep(0.01) 
 
 if __name__ == "__main__":
     # Point this to your latest model file, e.g., 'logs/franka_fast_reach/genesis_test/model_1500.pt'
-    CHECKPOINT = "logs/model_0.pt"
+    CHECKPOINT = "logs/model_250.pt"
     test_model(CHECKPOINT)
