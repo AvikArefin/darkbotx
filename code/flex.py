9692cca99d05d06b6922df769ed06f662dbd89a4
Avik Arefin <avikarefin5@gmail.com>
2026-03-08 03:14:55 +0600

    feat: test code for flex via ADS1115 and fsr via GPIO4
    flex sensor and force sensor are both resistive and can work with the same ADC (ADS1115).
    Flex has been coded to work with Analog to Digital Converter and communicate with Pi via I2C.
    FSR has been coded to work with GPIO4 of Pi directly (without any ADC)

---
+++ code/flex.py
@@ -0,0 +1,26 @@
+import time 
+import board 
+import busio
+
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Initialize I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+
+
+# Create the ADS object
+ads = ADS.ADS1115(i2c)
+ads.gain = 1
+
+# CORRECTED LINE: 
+
+# Since 'ADS' refers to the ads1115 module, 
+# use ADS.P0 if the library supports it there, 
+# or use the integer 0 directly which the library also accepts.
+chan = AnalogIn(ads, 0) 
+
+while True:
+    print(f"Flex / Force Sensor Voltage: {chan.voltage:.3f}V")
+    time.sleep(1)
