a8c0d60f09fc09f5d87d248eefeaa00733a42942
avik arefin <avik.me.arefin@gmail.com>
2026-07-14 05:01:19 +0600

    feat: add standalone sensor diagnostic script

--- src/sensor.py
+++ src/sensor.py
@@ -66,3 +66,14 @@ class Sensor:
     def get_all_voltages(self) -> list[float]:
         """Returns a list of voltages from all 4 pins."""
         return [self.get_voltage(i) for i in range(self.channels)]
+
+
+if __name__ == "__main__":
+    try:
+        import board
+        _i2c_bus = busio.I2C(board.SCL, board.SDA)
+        sensor = Sensor(channels=4, i2c=_i2c_bus)
+        print(sensor.get_all_voltages())
+
+    finally:
+        _i2c_bus.deinit()
